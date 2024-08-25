362572afe66c208aa5c8583eb3d7ac5077f3b689
juanalenca <juanbarros.dev@gmail.com>
2024-08-25 17:18:18 -0300

    feat: adiciona listagem de produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
+  products: any[] = [];
+  httpClient = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.httpClient.get<any[]>('/api/products').subscribe((products) => {
+      this.products = products;
+    });
+  }
+
+  trackById(index: number, product: any): number {
+    return product.id;
+  }
 }
